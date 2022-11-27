f3cfaf17e9e037dd1ce65c50ef647527aa7d88cc
Mohamed Rimshad <rimshadpcs@gmail.com>
2022-11-27 17:54:03 +0000

    Networking library with Retrofit

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/api/UnsplashApi.kt
@@ -0,0 +1,25 @@
+package com.codinginflow.imagesearchapp.api
+
+import com.codinginflow.imagesearchapp.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface UnsplashApi {
+    //getting api key from gradle local properties
+    companion object{
+        const val BASE_URL= "https://api.unsplash.com/"
+        const val CLIENT_ID = BuildConfig.UNSPLASH_ACCESS_KEY
+    }
+
+    //this case the header is mandatory for a request since api demands it, here its and api version and acces key
+    @Headers("Accept-Version: v1", "Authorization: Client-ID $CLIENT_ID")
+    @GET("search/photos")
+    suspend fun searchPhotos(
+       @Query("query")query: String,
+       @Query("page")page: Int,
+       @Query("per_page")perPage: Int
+    ): UnsplashResponse
+    //:UnsplashResponse is what we get from this request which is one object of photo
+
+}
