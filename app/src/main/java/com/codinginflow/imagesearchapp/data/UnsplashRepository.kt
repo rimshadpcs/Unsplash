84137bfbf83fcc8f1eee920dfc119d3b8d20afdd
Mohamed Rimshad <rimshadpcs@gmail.com>
2022-12-01 15:33:47 +0000

    Paging3,RecyclerView adapter

--- app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/data/UnsplashRepository.kt
@@ -1,11 +1,25 @@
 package com.codinginflow.imagesearchapp.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.codinginflow.imagesearchapp.api.UnsplashApi
+import retrofit2.http.Query
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
 
+    fun getSearchResult(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+
+            ),
+            pagingSourceFactory = {UnsplashPagingSource(unsplashApi,query)}
+        ).liveData
 
 }
